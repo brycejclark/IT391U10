cb66d6df929efe4107acaf3bbb2251d6d6ee53b7
Clark <clarbryc@amazon.com>
2020-10-07 02:36:03 -0500

    Add project files.

---
+++ IT391U7/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace IT391U7
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView txtNumber;
+
+        int number;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.main);
+
+            txtNumber = FindViewById<TextView>(Resource.Id.txtnumber);
+
+            FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) =>
+            txtNumber.Text = (++number).ToString();
+
+            FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) =>
+            txtNumber.Text = (--number).ToString();
+
+
+
+
+
+        }
+    }
+       
+}
